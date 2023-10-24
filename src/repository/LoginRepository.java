ce80f025d2649c7b5ed1e2a2bae34619adaf522e
huypd <huypd@10.12.19.185>
2023-10-24 14:44:49 +0700

    Login

---
+++ src/repository/LoginRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository;
+
+import dataAccess.LoginBankDao;
+import java.util.Locale;
+
+/**
+ *
+ * @author DiepTCNN
+ */
+public class LoginRepository implements ILoginRepository {
+
+    @Override
+    public void login(Locale language) {
+        LoginBankDao.Instance().login(language);
+    }  
+}
